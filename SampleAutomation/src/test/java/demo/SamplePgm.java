960e32e1fba88590409403870f8a04db0f47dce9
hp <hp@DESKTOP-LVBKE3A>
2022-09-21 11:53:12 +0530

    First Commit

---
+++ SampleAutomation/src/test/java/demo/SamplePgm.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.Test;
+
+public class SamplePgm {
+	@Test
+	public  void test() {
+		// TODO Auto-generated method stub
+System.out.println("Test method");
+	}
+
+}
---
+++ SampleAutomation/src/test/java/demo/Samplepgm1.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.junit.Test;
+
+public class Samplepgm1 {
+	@Test
+public void add() 
+  {
+	int a=10;
+	int b=20;
+	System.out.println(a+b);
+  }
+public void sub()
+  {
+	int c=23;
+	int d=43;
+	System.out.println(d-c);
+  }
+
+		// TODO Auto-generated method stub
+
+	}
+
+
